618441e13507f288a9a7711d67dfc884f16373c2
Carlos Echenique <carlos@echenique.com>
2024-02-14 12:19:17 -0500

    adding mechagon-gnome config

---
+++ mechagon-gnome/desktop-packages.nix
@@ -0,0 +1,47 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+# Plasma + Awesome, Bspwm, i3wm and more
+
+{
+  environment.systemPackages = with pkgs; [
+    amberol
+    blackbox-terminal
+    (catppuccin-gtk.override {
+      accents = [ "blue" ]; # You can specify multiple accents here to output multiple themes
+      size = "standard";
+      tweaks = [ "normal" ]; # You can also specify multiple tweaks here
+      variant = "mocha";
+    })
+    catppuccin-kvantum
+    chromium
+    discord
+    filezilla
+    firefox
+    fragments
+    gimp
+    gnome.gnome-tweaks
+    gnomeExtensions.appindicator
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.paperwm
+    inkscape
+    meld
+    obs-studio
+    obs-studio-plugins.wlrobs
+    ocs-url
+    papirus-icon-theme
+    qbittorrent-qt5
+    rapid-photo-downloader
+    slstatus
+    syncthingtray
+    telegram-desktop
+    thunderbird
+    # tokyo-night-gtk
+    trayscale
+    variety
+    wl-clipboard
+];
+}

150366f653727116319c977cd877119b1630636a
Carlos Echenique <carlos@echenique.com>
2024-03-16 12:39:33 -0400

    fixed avahi

--- mechagon-gnome/configuration.nix
+++ mechagon-gnome/configuration.nix
@@ -182,6 +182,10 @@ in {
     openFirewall = true;
     publish = {
       enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
       workstation = true;
     };
   };

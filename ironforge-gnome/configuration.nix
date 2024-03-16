5cd8117d5db1b963f06c56abc0d07cc7d0c69024
Carlos Echenique <carlos@echenique.com>
2024-03-16 12:44:42 -0400

    fixed avahi in ironforge

--- ironforge-gnome/configuration.nix
+++ ironforge-gnome/configuration.nix
@@ -184,6 +184,10 @@ in {
     openFirewall = true;
     publish = {
       enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
       workstation = true;
     };
   };

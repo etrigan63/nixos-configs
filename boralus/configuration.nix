5c0e95a1c189ae4c93642efc7c0644272ab50d14
etrigan63 <carlos@echenique.com>
2024-09-11 12:21:05 -0400

    Add Boralus configuration

---
+++ boralus/configuration.nix
@@ -0,0 +1,149 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./core-packages.nix
+      ./env-vars.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # Enable ZFS
+  #boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  #fileSystems."/mnt/tank" = {
+  #  device = "tank";
+  #  fsType = "zfs";
+  #};
+  #services.zfs.autoScrub.enable = true;
+
+  networking.hostName = "boralus"; # Define your hostname.
+  networking.hostId = "dbde4fe5";
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+ 
+  # Enable avahi
+  services.avahi = {
+    nssmdns4 = true;
+    enable = true;
+    ipv4 = true;
+    ipv6 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+  };
+
+  # Enable NFS
+  #services.nfs.server = {
+  #  enable = true;
+  #  exports = ''
+      # Export /mnt/tank
+  #    /mnt/tank		192.168.1.0/24(rw,crossmnt,sync,no_wdelay,no_root_squash,insecure,no_subtree_check)
+  #    '';
+  #};    
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable fish shell
+  programs.fish.enable = true;
+
+  # Enable syncthing
+  services = {
+    syncthing = {
+      enable = true;
+      user = "guru";
+      guiAddress = "0.0.0.0:8384";
+      dataDir = "/home/guru/Sync";
+      configDir = "/home/guru/.config/syncthing";
+    };
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.guru = {
+    isNormalUser = true;
+    description = "Carlos Echenique";
+    shell = pkgs.fish;
+    extraGroups = [ "networkmanager" "wheel" "storage" "docker" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+  
+  # Enable docker
+  virtualization.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
---
+++ boralus/core-packages.nix
@@ -0,0 +1,103 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+# never put virtualbox,mlocate in this list
+
+{
+  environment.systemPackages = with pkgs; [
+    #ckb-next
+    #google-chrome
+    #wpsoffice
+    #alacritty
+    #arandr
+    #arc-theme
+    avahi
+    baobab
+    bash-completion
+    bat
+    #betterdiscordctl
+    #bibata-cursors
+    #blueberry
+    bottom
+    #chromium
+    curl
+    #desktop-file-utils
+    #dex
+    #discord
+    #dmenu
+    #dropbox
+    duf
+    edid-decode
+    #etcher
+    #evince
+    #feh
+    #filezilla
+    #firefox
+    fish
+    fishPlugins.tide
+    #font-manager
+    fsearch
+    #gimp
+    git
+    gitui
+    #gnome.dconf-editor
+    #gnome.gnome-disk-utility
+    #gnome.gvfs
+    #hack-font
+    #hblock
+    hw-probe
+    hwinfo
+    #i3lock-fancy
+    #inkscape
+    #insync
+    inxi
+    killall
+    #kitty
+    libsecret
+    lm_sensors
+    lshw
+    #meld
+    most
+    fastfetch
+    neovim
+    (nerdfonts.override { fonts = [ "RobotoMono"]; })
+    #networkmanagerapplet
+    nixos-generators
+    (nnn.override { withNerdIcons = true; })
+    nodejs
+    nodePackages_latest.npm
+    #nomacs
+    #ocl-icd
+    #obs-studio
+    #ookla-speedtest
+    #qbittorrent-qt5
+    procps
+    python3
+    python311Packages.pynvim
+    #qt6Packages.qtstyleplugin-kvantum
+    #rapid-photo-downloader
+    ripgrep
+    #scrot
+    #sddm-chili-theme
+    #simplescreenrecorder
+    #spotify
+    #telegram-desktop
+    #thunderbird
+    tree
+    unzip
+    #variety
+    #vlc
+    #volumeicon
+    #vscode
+    wget
+    #widevine-cdm
+    #xorg.xkill
+    zig
+    #zsh
+    #zsh-completions
+    #zsh-syntax-highlighting
+  ];
+}
